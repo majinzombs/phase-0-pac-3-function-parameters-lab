c1328345c7813ca7aec7c2b2e4bd8b929446f10e
Jonathan Koban <jon.koban@outlook.com>
2022-02-28 11:20:03 -0500

    commit tree a7136e10c5a2ec739b66b88d120d888efdc0a099

---
+++ index.js
@@ -0,0 +1,11 @@
+function introduction(name = 'AKI'){
+   return `Hi, my name is ${name}.`
+}
+
+function introductionWithLanguage(name = 'AKI', language = 'Ember.js'){
+    return `Hi, my name is ${name} and I am learning to program in ${language}.`
+ }
+
+ function introductionWithLanguageOptional(name = 'Gracie', language = 'JavaScript'){
+    return `Hi, my name is ${name} and I am learning to program in ${language}.`
+ }
